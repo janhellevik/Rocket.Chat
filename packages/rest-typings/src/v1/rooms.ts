ff2748b8b3204973be0b5d66d621abdd34226fa6
Abhinav Kumar <abhinav@avitechlab.com>
2025-01-10 20:06:02 +0530

    using rolePriority value to sort
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- packages/rest-typings/src/v1/rooms.ts
+++ packages/rest-typings/src/v1/rooms.ts
@@ -615,12 +615,12 @@ const roomsOpenSchema = {
 };
 
 export const isRoomsOpenProps = ajv.compile<RoomsOpenProps>(roomsOpenSchema);
+
 type MembersOrderedByRoleProps = {
 	roomId?: IRoom['_id'];
 	roomName?: IRoom['name'];
 	status?: string[];
 	filter?: string;
-	rolesOrder?: IRole['_id'][];
 };
 
 export type RoomsMembersOrderedByRoleProps = PaginatedRequest<MembersOrderedByRoleProps>;
@@ -632,13 +632,6 @@ const membersOrderedByRoleRolePropsSchema = {
 		},
 		roomName: {
 			type: 'string',
-		},
-		rolesOrder: {
-			type: 'array',
-			items: {
-				type: 'string',
-			},
-			nullable: true,
 		},
 		status: {
 			type: 'array',
